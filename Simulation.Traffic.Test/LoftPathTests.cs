34dce75006f3fa660ccf5dfa6e1a1f7cb8c16fa7
Stijn Tallon <stijn_tallon@msn.com>
2018-01-18 21:34:54 +0100

    lofthpath tests

--- Simulation.Traffic.Test/LoftPathTests.cs
+++ Simulation.Traffic.Test/LoftPathTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Simulation.Traffic.Lofts;
+using Simulation.Traffic.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,8 +14,72 @@ namespace Simulation.Traffic.Test
     public class LoftPathTests
     {
         [TestMethod]
+        public void TestPositiveArc()
+        {
+            var arc = new ArcLoftPath(Vector3.left, Mathf.PI / 2, Vector3.zero, Vector3.up);
+
+            Assert.AreEqual(Mathf.PI / 2, arc.Length);
+            var startTransform = arc.GetTransform(0);
+
+            var endTransform = arc.GetTransform(arc.Length);
+
+            var startForward = startTransform.MultiplyVector(Vector3.forward);
+            var endForward = endTransform.MultiplyVector(Vector3.forward);
+
+            var startPosition = startTransform.MultiplyPoint3x4(Vector3.zero);
+            var endPosition = endTransform.MultiplyPoint3x4(Vector3.zero);
+
+            Assert.AreEqual(Vector3.left, startPosition.Round(0.00001f));
+            Assert.AreEqual(Vector3.forward, endPosition.Round(0.00001f));
+
+            Assert.AreEqual(Vector3.forward, startForward.Round(0.00001f));
+            Assert.AreEqual(Vector3.right, endForward.Round(0.00001f));
+
+            var halfTransform = arc.GetTransform(arc.Length / 2);
+
+
+            var halfForward = new Vector3(1, 0, 1).normalized;
+
+            Assert.AreEqual(halfForward.Round(0.00001f), halfTransform.MultiplyVector(Vector3.forward).Round(0.00001f));
+
+        }
+
+        [TestMethod]
+        public void TestNegativeArc()
+        {
+            var arc = new ArcLoftPath(Vector3.left, -Mathf.PI / 2, Vector3.zero, Vector3.up);
+
+            Assert.AreEqual(Mathf.PI / 2, arc.Length);
+            var startTransform = arc.GetTransform(0);
+
+            var endTransform = arc.GetTransform(arc.Length);
+
+            var startForward = startTransform.MultiplyVector(Vector3.forward);
+            var endForward = endTransform.MultiplyVector(Vector3.forward);
+
+            var startPosition = startTransform.MultiplyPoint3x4(Vector3.zero);
+            var endPosition = endTransform.MultiplyPoint3x4(Vector3.zero);
+
+            Assert.AreEqual(Vector3.left, startPosition.Round(0.00001f));
+            Assert.AreEqual(Vector3.back, endPosition.Round(0.00001f));
+
+            Assert.AreEqual(Vector3.back, startForward.Round(0.00001f));
+            Assert.AreEqual(Vector3.right, endForward.Round(0.00001f));
+
+            var halfTransform = arc.GetTransform(arc.Length / 2);
+
+
+            var halfForward = new Vector3(1, 0, -1).normalized;
+
+            Assert.AreEqual(halfForward.Round(0.00001f), halfTransform.MultiplyVector(Vector3.forward).Round(0.00001f));
+
+        }
+
+
+
+        [TestMethod]
         public void TestLinearX()
-        { 
+        {
             var linearLoftPath = new LinearPath(Vector3.zero, new Vector3(1, 0, 0));
 
             var transformStart = linearLoftPath.GetTransform(0);
---
+++ Simulation.Traffic/Utilities/VectorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Simulation.Traffic.Utilities
+{
+    public static class VectorExtensions
+    {
+        public static Vector3 Round(this Vector3 input)
+        {
+            return new Vector3(Mathf.Round(input.x), Mathf.Round(input.y), Mathf.Round(input.z));
+        }
+        public static Vector3 Round(this Vector3 input, float scale)
+        {
+            input /= scale;
+            return scale * new Vector3(Mathf.Round(input.x), Mathf.Round(input.y), Mathf.Round(input.z));
+        }
+    }
+}
